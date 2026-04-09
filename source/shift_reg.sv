8323f7a88b64fde63004d7c7d902e6a1c0a45fdf
Anna Dalton <annazd06@gmail.com>
2026-04-09 17:54:52 -0400

    adding code for counter and shift register

--- source/shift_reg.sv
+++ source/shift_reg.sv
@@ -1,12 +1,45 @@
 `timescale 1ns / 10ps
 
 module shift_reg #(
-    // parameters
+    parameter SIZE = 8,
+    parameter MSB_FIRST = 0
 ) (
-    input logic clk, n_rst
+    input logic clk, n_rst, shift_enable, serial_in, load_enable,
+    input logic [SIZE-1 :0] parallel_in,
+    output logic serial_out,
+    output logic [SIZE-1 :0] parallel_out
 );
 
+    logic next_serial_out;
+    logic [SIZE-1:0] next_parallel_out; 
 
+    always_ff @(posedge clk, negedge n_rst) begin
+        if(~n_rst) begin
+            parallel_out <= '1;
+        end else begin
+            parallel_out <= next_parallel_out;
+        end
+    end
 
+    always_comb begin 
+        if (load_enable) begin
+            next_parallel_out = parallel_in;
+        end else if (shift_enable) begin
+            if (MSB_FIRST == 0) begin //lsb first
+                next_parallel_out = {serial_in, parallel_out[SIZE - 1:1]};
+            end else begin //msb first
+                next_parallel_out = {parallel_out[SIZE - 1:0], serial_in};
+            end
+        end else begin // hold old values
+            next_parallel_out = parallel_out;
+        end
+    end
+
+    always_comb begin
+        if (MSB_FIRST) begin
+            serial_out = parallel_out[SIZE];
+        end else begin
+            serial_out = parallel_out[0];
+        end
+    end
 endmodule
-
