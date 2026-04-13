00e8638f814fae9a2c188a7b4cb3bacff94bae57
337mg078 <337mg078@ececomp5.ecn.purdue.edu>
2026-04-12 23:46:44 -0400

    new files for configuration

---
+++ source/irq_status_control.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 10ps
+
+module irq_status_control #(
+    // parameters
+) (
+    input logic clk, n_rst
+);
+
+
+
+endmodule
+
---
+++ source/register_bank.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 10ps
+
+module register_bank #(
+    // parameters
+) (
+    input logic clk, n_rst
+);
+
+
+
+endmodule
+
